988fd37ca4575cb3068127cac7cdb765b207fd61
Grimm <fvckgrimm@protonmail.ch>
2025-12-06 22:32:25 -0800

    feat: close to sys tray

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -11,18 +11,81 @@ mod commands {
 
 use commands::{mining, node, wallet};
 use state::NodeState;
-use tauri::{Manager, RunEvent};
+use tauri::{
+    menu::{Menu, MenuItem},
+    tray::{MouseButton, MouseButtonState, TrayIconBuilder, TrayIconEvent},
+    Manager, RunEvent, WindowEvent,
+};
 
 fn main() {
     let app = tauri::Builder::default()
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_dialog::init())
         .manage(NodeState::new())
+        .setup(|app| {
+            // Create tray menu items
+            let quit = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>)?;
+            let show = MenuItem::with_id(app, "show", "Show Window", true, None::<&str>)?;
+            let hide = MenuItem::with_id(app, "hide", "Hide Window", true, None::<&str>)?;
+
+            // Create the menu
+            let menu = Menu::with_items(app, &[&show, &hide, &quit])?;
+
+            // Build the tray icon
+            let _tray = TrayIconBuilder::with_id("main-tray")
+                .tooltip("Juno Cash Wallet")
+                .icon(app.default_window_icon().unwrap().clone())
+                .menu(&menu)
+                .show_menu_on_left_click(false)
+                .on_menu_event(move |app, event| match event.id.as_ref() {
+                    "quit" => app.exit(0),
+                    "show" => {
+                        if let Some(window) = app.get_webview_window("main") {
+                            let _ = window.show();
+                            let _ = window.set_focus();
+                        }
+                    }
+                    "hide" => {
+                        if let Some(window) = app.get_webview_window("main") {
+                            let _ = window.hide();
+                        }
+                    }
+                    _ => {}
+                })
+                .on_tray_icon_event(|tray, event| {
+                    if let TrayIconEvent::Click {
+                        button: MouseButton::Left,
+                        button_state: MouseButtonState::Up,
+                        ..
+                    } = event
+                    {
+                        let app = tray.app_handle();
+                        if let Some(webview_window) = app.get_webview_window("main") {
+                            if webview_window.is_visible().unwrap_or(false) {
+                                let _ = webview_window.hide();
+                            } else {
+                                let _ = webview_window.show();
+                                let _ = webview_window.set_focus();
+                            }
+                        }
+                    }
+                })
+                .build(app)?;
+
+            Ok(())
+        })
+        .on_window_event(|window, event| {
+            // Intercept close event and hide window instead
+            if let WindowEvent::CloseRequested { api, .. } = event {
+                window.hide().unwrap();
+                api.prevent_close();
+            }
+        })
         .invoke_handler(tauri::generate_handler![
             // Node Launcher
             node::launch_node,
             node::stop_node,
-            node::get_default_juno_paths, // This will work now
+            node::get_default_juno_paths,
             // Mining
             mining::get_mining_info,
             mining::get_block_subsidy,
@@ -36,7 +99,6 @@ fn main() {
             wallet::get_block_count,
             wallet::get_all_addresses,
         ])
-        //.run(tauri::generate_context!())
         .build(tauri::generate_context!())
         .expect("error while running tauri application");
 
@@ -47,7 +109,6 @@ fn main() {
                 // Get the state, lock the mutex, and kill the child process if it exists.
                 let state = app_handle.state::<NodeState>();
                 let mut guard = state.process.lock().unwrap();
-
                 if let Some(mut child) = guard.take() {
                     println!("Tauri is exiting. Killing Juno Node...");
                     let _ = child.kill(); // Force kill the process
